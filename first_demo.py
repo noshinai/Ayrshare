69bba31a8345ff0d205b1aa97b08b64b0937aa07
unknown <noshin@factiiv.io>
2025-06-28 13:46:44 +0600

    seperate env

--- first_demo.py
+++ first_demo.py
@@ -1,14 +1,26 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+# Load variables from .env file
+load_dotenv()
+
+# Get the API key from environment
+api_key = os.getenv("AYRSHARE_API_KEY")
+
+# Make sure it's loaded
+if not api_key:
+    raise ValueError("AYRSHARE_API_KEY is not set in the .env file")
 
 url = "https://api.ayrshare.com/api/post"
 headers = {
-"Authorization": "Bearer 51B2019D-8F244E99-B1CAF823-3BD8994F",
-"Content-Type": "application/json"
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
 }
 data = {
-"post": "Good Morning", #Mandatory
-"platforms": ["linkedin", "facebook"],
-"mediaUrls": ["https://img.ayrshare.com/012/gb.jpg"]
+    "post": "Good Morning",  # Mandatory
+    "platforms": ["linkedin", "facebook"],
+    "mediaUrls": ["https://img.ayrshare.com/012/gb.jpg"]
 }
 
 response = requests.post(url, json=data, headers=headers)
